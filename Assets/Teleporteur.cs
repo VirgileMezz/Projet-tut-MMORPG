d7a44cd80e136c4277d8224613cafdc2cf64d02c
VirgileMezz <virgile.mezzarobba@laposte.net>
2018-10-17 10:56:37 +0200

    ajout teleporteur verslascene de combat dans l'edifice de pierre et du controller de personnage dans la mainscene

---
+++ Assets/Teleporteur.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Teleporteur : MonoBehaviour {
+
+    [SerializeField] private int sceneIndex;
+	
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(sceneIndex, LoadSceneMode.Single);
+    }
+}
